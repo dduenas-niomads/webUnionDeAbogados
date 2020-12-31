2ffda5b106548654087af3eb6d4a8208d5a3e502
Alvaro Acosta <amacostas92@gmail.com>
2020-12-30 22:59:22 -0500

    cambios 30dic mail contacto2

--- sendemail.php
+++ sendemail.php
@@ -1,24 +1,22 @@
 <?php
-$to = "danielgrillz@gmail.com, info@udayaf.com";
+$to = "amacoast92@gmail.com";
 $subject = "Contacto de union de abogados y asesores financieros";
-
-$message = "
-<html>
-<head>
-<title>Este es un correo de contacto</title>
-</head>
-<body>
-<p>Test email. Please ignore.</p>
-</body>
-</html>
-";
-
+$message = '<html><body>';
+$message .= '<table rules="all" style="border-color: #666;" cellpadding="10">';
+$message .= "<tr style='background: #eee;'><td><strong>Nombre:</strong> </td><td>" . strip_tags($_POST['name']) . "</td></tr>";
+$message .= "<tr><td><strong>Email:</strong> </td><td>" . strip_tags($_POST['phone']) . "</td></tr>";
+$message .= "<tr style='background: #eee;'><td><strong>Mensaje:</strong> </td><td>" . strip_tags($_POST['message']) . "</td></tr>";
+$message .= "</table>";
+$message .= "</body></html>";
 // It is mandatory to set the content-type when sending HTML email
 $headers = "MIME-Version: 1.0" . "\r\n";
 $headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
 
 // More headers. From is required, rest other headers are optional
-$headers .= 'From: <info@example.com>' . "\r\n";
+$headers .= 'From: '. strip_tags($_POST['phone']) . "\r\n";
 
-mail($to,$subject,$message,$headers);
+$mail = mail($to,$subject,$message,$headers);
+if ($mail){
+    echo "<h4>Mail enviado</h4>";
+}
 ?>
